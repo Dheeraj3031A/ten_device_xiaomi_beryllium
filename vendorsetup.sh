e8a4647cc198c4bf6e4d265c5b3e431c156c9d32
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-09-11 18:15:44 +0530

    beryllium: include hardware_xiaomi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,3 +13,7 @@ git clone https://github.com/Dheeraj3031A/kernel_xiaomi_beryllium.git -b 14.0-ks
 # MiCam
 rm -rf vendor/miuicamera
 git clone https://github.com/Legendleo90/vendor_miuicamera.git -b fourteen vendor/miuicamera --depth=1
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
